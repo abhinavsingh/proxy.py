af572111e093c8b00af03998f663599d134c5fcc
Abhinav Singh <mailsforabhinav@gmail.com>
2019-10-02 02:49:49 -0700

    Attempt to open devtools

--- app/main.js
+++ app/main.js
@@ -1,10 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const http = require('http');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
-function createWindow () {
+const httpGet = url => {
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            res.setEncoding('utf8');
+            let body = '';
+            res.on('data', chunk => body += chunk);
+            res.on('end', () => resolve(JSON.parse(body)));
+        }).on('error', reject);
+    });
+};
+
+async function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({
         width: 800,
@@ -12,44 +22,36 @@ function createWindow () {
         webPreferences: {
             nodeIntegration: true
         }
-    })
+    });
 
-    // and load the index.html of the app.
-    // win.loadFile('index.html')
+    win.webContents.openDevTools();
 
-    // Open the DevTools.
-    win.webContents.openDevTools()
+    let json = await httpGet('http://localhost:5858/json');
+    let devtoolsFrontendUrlCompat = json[0]['devtoolsFrontendUrlCompat'];
+    let webSocketDebuggerUrl = json[0]['webSocketDebuggerUrl'];
+    win.loadURL(devtoolsFrontendUrlCompat);
+    console.log(devtoolsFrontendUrlCompat);
+    console.log(webSocketDebuggerUrl);
 
     // Emitted when the window is closed.
     win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         win = null
     })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+app.on('activate', async () => {
     if (win === null) {
-        createWindow()
+        await createWindow();
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
