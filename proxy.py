b6b0a62bda2bed9b4537801e8eb702ab348e1ba0
Abhinav Singh <mailsforabhinav@gmail.com>
2019-10-02 21:34:34 -0700

    Add support for building websocket frames

--- proxy.py
+++ proxy.py
@@ -14,6 +14,7 @@ import datetime
 import errno
 import hashlib
 import importlib
+import io
 import inspect
 import ipaddress
 import logging
@@ -25,6 +26,7 @@ import secrets
 import selectors
 import socket
 import ssl
+import struct
 import subprocess
 import sys
 import threading
@@ -164,6 +166,16 @@ HttpProtocolTypes = NamedTuple('HttpProtocolTypes', [
     ('WEBSOCKET', int),
 ])
 httpProtocolTypes = HttpProtocolTypes(1, 2, 3)
+
+WebsocketOpcodes = NamedTuple('WebsocketOpcodes', [
+    ('CONTINUATION_FRAME', int),
+    ('TEXT_FRAME', int),
+    ('TEXT_BINARY', int),
+    ('CONNECTION_CLOSE', int),
+    ('PING', int),
+    ('PONG', int),
+])
+websocketOpcodes = WebsocketOpcodes(0x0, 0x1, 0x2, 0x8, 0x9, 0xA)
 
 
 def build_http_request(method: bytes, url: bytes,
@@ -1316,28 +1328,102 @@ class WebsocketFrame:
         self.rsv3: bool = False
         self.opcode: int = 0
         self.masked: bool = False
-        self.payload_length: int = 0
+        self.payload_length: Optional[int] = None
         self.mask: Optional[bytes] = None
         self.data: Optional[bytes] = None
 
+    def parse_fin_and_rsv(self, byte: int) -> None:
+        self.fin = bool(byte & 1 << 7)
+        self.rsv1 = bool(byte & 1 << 6)
+        self.rsv2 = bool(byte & 1 << 5)
+        self.rsv3 = bool(byte & 1 << 4)
+        self.opcode = byte & 0b00001111
+
+    def parse_mask_and_payload(self, byte: int) -> None:
+        self.masked = bool(byte & 0b10000000)
+        self.payload_length = byte & 0b01111111
+
+    def build(self) -> bytes:
+        if self.payload_length is None and self.data:
+            self.payload_length = len(self.data)
+        raw = io.BytesIO()
+        raw.write(
+            struct.pack(
+                '!B',
+                (1 << 7 if self.fin else 0) |
+                (1 << 6 if self.rsv1 else 0) |
+                (1 << 5 if self.rsv2 else 0) |
+                (1 << 4 if self.rsv3 else 0) |
+                self.opcode
+            ))
+        assert self.payload_length is not None
+        if self.payload_length < 126:
+            raw.write(
+                struct.pack(
+                    '!B',
+                    (1 << 7 if self.masked else 0) | self.payload_length
+                )
+            )
+        elif self.payload_length < 1 << 16:
+            raw.write(
+                struct.pack(
+                    '!BH',
+                    (1 << 7 if self.masked else 0) | 126,
+                    self.payload_length
+                )
+            )
+        elif self.payload_length < 1 << 64:
+            raw.write(
+                struct.pack(
+                    '!BHQ',
+                    (1 << 7 if self.masked else 0) | 127,
+                    self.payload_length
+                )
+            )
+        else:
+            raise ValueError(f'Invalid payload_length { self.payload_length },'
+                             f'maximum allowed { 1 << 64 }')
+        if self.masked and self.data:
+            mask = secrets.token_bytes(4) if self.mask is None else self.mask
+            raw.write(mask)
+            raw.write(self.apply_mask(self.data, mask))
+        elif self.data:
+            raw.write(self.data)
+        return raw.getvalue()
+
     def parse(self, raw: bytes) -> None:
-        self.fin = bool(raw[0] & 1 << 7)
-        self.rsv1 = bool(raw[0] & 1 << 6)
-        self.rsv2 = bool(raw[0] & 1 << 5)
-        self.rsv3 = bool(raw[0] & 1 << 4)
-        self.opcode = raw[0] & 0b00001111
-        self.masked = bool(raw[1] & 0b10000000)
-        # TODO: Handle case when payload length is 126 or 127
-        self.payload_length = raw[1] & 0b01111111
+        cur = 0
+        self.parse_fin_and_rsv(raw[cur])
+        cur += 1
+
+        self.parse_mask_and_payload(raw[cur])
+        cur += 1
+
+        if self.payload_length == 126:
+            data = raw[cur: cur + 2]
+            self.payload_length, = struct.unpack('!H', data)
+            cur += 2
+        elif self.payload_length == 127:
+            data = raw[cur: cur + 8]
+            self.payload_length, = struct.unpack('!Q', data)
+            cur += 8
+
         if self.masked:
-            self.mask = raw[2:6]
-        self.data = raw[6:]
+            self.mask = raw[cur: cur + 4]
+            cur += 4
+
+        self.data = raw[cur:]
+
         if self.masked:
             assert self.mask is not None
-            data = bytearray(self.data)
-            for i in range(len(self.data)):
-                data[i] = data[i] ^ self.mask[i % 4]
-            self.data = bytes(data)
+            self.data = self.apply_mask(self.data, self.mask)
+
+    @staticmethod
+    def apply_mask(data: bytes, mask: bytes) -> bytes:
+        raw = bytearray(data)
+        for i in range(len(raw)):
+            raw[i] = raw[i] ^ mask[i % 4]
+        return bytes(raw)
 
     @staticmethod
     def key_to_accept(key: bytes) -> bytes:
