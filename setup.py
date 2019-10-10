44fc0959f3c2c234b778fc0d9fd9fa6a9944251e
Abhinav Singh <mailsforabhinav@gmail.com>
2019-10-10 15:07:01 -0700

    Add setup.py under lint workflow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
     proxy.py
     ~~~~~~~~
     ⚡⚡⚡ Fast, Lightweight, Programmable Proxy Server in a single Python file.
-    
+
     :copyright: (c) 2013-present by Abhinav Singh and contributors.
     :license: BSD, see LICENSE for more details.
 """
