d8a9baad1a030154295c6ca0428de2ebee81571a
Abhinav Singh <mailsforabhinav@gmail.com>
2019-10-10 19:58:40 -0700

    Lint fix

--- proxy.py
+++ proxy.py
@@ -710,8 +710,8 @@ class HttpParser:
 
     def is_http_1_1_keep_alive(self) -> bool:
         return self.version == HTTP_1_1 and \
-               (not self.has_header(b'Connection') or
-                self.header(b'Connection').lower() == b'keep-alive')
+            (not self.has_header(b'Connection') or
+             self.header(b'Connection').lower() == b'keep-alive')
 
 
 class AcceptorPool:
