8c8ed566e349d63d479339f490d0bf944f078e1c
Abhinav Singh <mailsforabhinav@gmail.com>
2019-10-20 23:16:11 -0700

    Update dashboard for refactored imports

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,40 +1,46 @@
 """
-    proxy.py
+    py
     ~~~~~~~~
     ⚡⚡⚡ Fast, Lightweight, Programmable Proxy Server in a single Python file.
 
     :copyright: (c) 2013-present by Abhinav Singh and contributors.
     :license: BSD, see LICENSE for more details.
 
-    app.py extends proxy.py APIs to provide a frontend dashboard
+    app.py extends py APIs to provide a frontend dashboard
     available at http://localhost:8899/index.html
 """
-
 import json
+import logging
 from typing import List, Tuple
 
-import proxy
+from core.web_server import HttpWebServerPlugin, HttpWebServerBasePlugin, httpProtocolTypes
+from core.http_parser import HttpParser
+from core.utils import text_, build_http_response, bytes_
+from core.websocket import WebsocketFrame
+from core.status_codes import httpStatusCodes
+
+logger = logging.getLogger(__name__)
 
 
-class ProxyDashboard(proxy.HttpWebServerBasePlugin):
+class ProxyDashboard(HttpWebServerBasePlugin):
 
     def routes(self) -> List[Tuple[int, bytes]]:
         return [
-            (proxy.httpProtocolTypes.HTTP, b'/'),
-            (proxy.httpProtocolTypes.HTTPS, b'/'),
-            (proxy.httpProtocolTypes.HTTP, b'/proxy.html'),
-            (proxy.httpProtocolTypes.HTTPS, b'/proxy.html'),
-            (proxy.httpProtocolTypes.WEBSOCKET, b'/app'),
+            (httpProtocolTypes.HTTP, b'/'),
+            (httpProtocolTypes.HTTPS, b'/'),
+            (httpProtocolTypes.HTTP, b'/html'),
+            (httpProtocolTypes.HTTPS, b'/html'),
+            (httpProtocolTypes.WEBSOCKET, b'/app'),
         ]
 
-    def handle_request(self, request: proxy.HttpParser) -> None:
+    def handle_request(self, request: HttpParser) -> None:
         if request.path == b'/':
             self.client.queue(
-                proxy.HttpWebServerPlugin.read_and_build_static_file_response(
-                    self.config.static_server_dir + proxy.text_(b'/proxy.html')))
-        elif request.path == b'/proxy.html':
-            self.client.queue(proxy.build_http_response(
-                proxy.httpStatusCodes.PERMANENT_REDIRECT, reason=b'Permanent Redirect',
+                HttpWebServerPlugin.read_and_build_static_file_response(
+                    self.config.static_server_dir + text_(b'/html')))
+        elif request.path == b'/html':
+            self.client.queue(build_http_response(
+                httpStatusCodes.PERMANENT_REDIRECT, reason=b'Permanent Redirect',
                 headers={
                     b'Location': b'/',
                     b'Content-Length': b'0',
@@ -43,27 +49,27 @@ class ProxyDashboard(proxy.HttpWebServerBasePlugin):
             ))
 
     def on_websocket_open(self) -> None:
-        proxy.logger.info('app ws opened')
+        logger.info('app ws opened')
 
-    def on_websocket_message(self, frame: proxy.WebsocketFrame) -> None:
+    def on_websocket_message(self, frame: WebsocketFrame) -> None:
         try:
             message = json.loads(frame.data)
         except UnicodeDecodeError:
-            proxy.logger.error(frame.data)
-            proxy.logger.info(frame.opcode)
+            logger.error(frame.data)
+            logger.info(frame.opcode)
             return
 
         if message['method'] == 'ping':
             self.reply_pong(message['id'])
         else:
-            proxy.logger.info(frame.data)
-            proxy.logger.info(frame.opcode)
+            logger.info(frame.data)
+            logger.info(frame.opcode)
 
     def on_websocket_close(self) -> None:
-        proxy.logger.info('app ws closed')
+        logger.info('app ws closed')
 
     def reply_pong(self, idd: int):
         self.client.queue(
-            proxy.WebsocketFrame.text(
-                proxy.bytes_(
+            WebsocketFrame.text(
+                bytes_(
                     json.dumps({'id': idd, 'response': 'pong'}))))
