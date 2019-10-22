bc08b0666edc7a02acd5cdd57c748c6c776e56cb
Abhinav Singh <mailsforabhinav@gmail.com>
2019-10-21 22:41:15 -0700

    Github Zip downloads cannot be executed as Github puts project under a folder named after Github project, this breaks python interpreter expectation of finding a __main__.py in the root directory

--- proxy/__main__.py
+++ proxy/__main__.py
@@ -7,7 +7,7 @@
     :copyright: (c) 2013-present by Abhinav Singh and contributors.
     :license: BSD, see LICENSE for more details.
 """
-from proxy.main import entry_point
+from .main import entry_point
 
 
 if __name__ == '__main__':
