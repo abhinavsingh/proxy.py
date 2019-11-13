e6cb683a8311c871ea3076357d4d80b6c6571af0
Abhinav Singh <mailsforabhinav@gmail.com>
2019-11-12 17:09:16 -0800

    make devtools

--- dashboard/src/core/devtools.ts
+++ dashboard/src/core/devtools.ts
@@ -7,32 +7,30 @@
     :copyright: (c) 2013-present by Abhinav Singh and contributors.
     :license: BSD, see LICENSE for more details.
 */
-const path = require('path')
-const fs = require('fs')
+import path = require('path')
+import fs = require('fs')
 const ncp = require('ncp').ncp
+
 ncp.limit = 16
 
-const publicFolderPath = path.join(__dirname, 'public')
-const destinationFolderPath = path.join(publicFolderPath, 'devtools')
+function setUpDevTools () {
+  const destinationFolderPath = path.join(path.dirname(path.dirname(__dirname)), 'public', 'dashboard', 'devtools')
 
-const publicFolderExists = fs.existsSync(publicFolderPath)
-if (!publicFolderExists) {
-  console.error(publicFolderPath + ' folder doesn\'t exist, make sure you are in the right directory.')
-  process.exit(1)
-}
-
-const destinationFolderExists = fs.existsSync(destinationFolderPath)
-if (!destinationFolderExists) {
-  console.error(destinationFolderPath + ' folder doesn\'t exist, make sure you are in the right directory.')
-  process.exit(1)
-}
-
-const chromeDevTools = path.dirname(require.resolve('chrome-devtools-frontend/front_end/inspector.html'))
-
-console.log(chromeDevTools + ' ---> ' + destinationFolderPath)
-ncp(chromeDevTools, destinationFolderPath, (err: any) => {
-  if (err) {
-    return console.error(err)
+  const destinationFolderExists = fs.existsSync(destinationFolderPath)
+  if (!destinationFolderExists) {
+    console.error(destinationFolderPath + ' folder doesn\'t exist, make sure you are in the right directory.')
+    process.exit(1)
   }
-  console.log('Copy successful!!!')
-})
+
+  const chromeDevTools = path.dirname(require.resolve('chrome-devtools-frontend/front_end/inspector.html'))
+
+  console.log(chromeDevTools + ' ---> ' + destinationFolderPath)
+  ncp(chromeDevTools, destinationFolderPath, (err: any) => {
+    if (err) {
+      return console.error(err)
+    }
+    console.log('Copy successful!!!')
+  })
+}
+
+setUpDevTools()
