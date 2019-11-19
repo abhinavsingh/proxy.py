ad42e0d74df4917b70c2cac3249e0133a3f197d2
Abhinav Singh <mailsforabhinav@gmail.com>
2019-11-18 20:45:51 -0800

    Doc & Banner update to match GitHub (#182)
    * Update doc and banner
    
    * Update banner to match GitHub
    
    * Update older banners too
    
    * Add update_desc to .gitignore
    
    * Update banner for dashboard to match github
    
    * also update html, js, css

--- proxy/http/websocket.py
+++ proxy/http/websocket.py
@@ -2,7 +2,8 @@
 """
     proxy.py
     ~~~~~~~~
-    ⚡⚡⚡ Fast, Lightweight, Programmable Proxy Server in a single Python file.
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
 
     :copyright: (c) 2013-present by Abhinav Singh and contributors.
     :license: BSD, see LICENSE for more details.
