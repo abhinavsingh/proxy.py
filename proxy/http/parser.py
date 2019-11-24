0d83dac5f06cdcbfa36ccb93d7142f3d17f2a627
Abhinav Singh <mailsforabhinav@gmail.com>
2019-11-24 01:06:21 -0800

    Clarity in test comments

--- proxy/http/parser.py
+++ proxy/http/parser.py
@@ -129,11 +129,10 @@ class HttpParser:
 
         Check HttpParser state after parse has successfully returned."""
         self.total_size += len(raw)
-
-        # Prepend past buffer
         raw = self.buffer + raw
         self.buffer = b''
 
+        # TODO(abhinavsingh): Someday clean this up.
         more = True if len(raw) > 0 else False
         while more and self.state != httpParserStates.COMPLETE:
             if self.state in (
@@ -164,7 +163,6 @@ class HttpParser:
 
     def process(self, raw: bytes) -> Tuple[bool, bytes]:
         """Returns False when no CRLF could be found in received bytes."""
-        ends_with_crlf = raw.endswith(CRLF)
         line, raw = find_http_line(raw)
         if line is None:
             return False, raw
@@ -181,21 +179,25 @@ class HttpParser:
             else:
                 self.process_header(line)
 
-        # When connect request is received without a following host header
-        # See
-        # `TestHttpParser.test_connect_request_without_host_header_request_parse`
-        # for details
+        # TODO(abhinavsingh): Can these be generalized instead of per-case handling?
+        # When server sends a response line without any header or body e.g.
+        # HTTP/1.1 200 Connection established\r\n\r\n
         if self.state == httpParserStates.LINE_RCVD and \
                 self.type == httpParserTypes.RESPONSE_PARSER and \
                 raw == CRLF:
             self.state = httpParserStates.COMPLETE
+        # When connect request is received without a following host header
+        # See
+        # `TestHttpParser.test_connect_request_without_host_header_request_parse`
+        # for details
+        #
         # When raw request has ended with \r\n\r\n and no more http headers are expected
         # See `TestHttpParser.test_request_parse_without_content_length` and
         # `TestHttpParser.test_response_parse_without_content_length` for details
         elif self.state == httpParserStates.HEADERS_COMPLETE and \
                 self.type == httpParserTypes.REQUEST_PARSER and \
                 self.method != httpMethods.POST and \
-                ends_with_crlf:
+                raw == b'':
             self.state = httpParserStates.COMPLETE
         elif self.state == httpParserStates.HEADERS_COMPLETE and \
                 self.type == httpParserTypes.REQUEST_PARSER and \
@@ -204,7 +206,7 @@ class HttpParser:
                 (b'content-length' not in self.headers or
                  (b'content-length' in self.headers and
                   int(self.headers[b'content-length'][1]) == 0)) and \
-                ends_with_crlf:
+                raw == b'':
             self.state = httpParserStates.COMPLETE
 
         return len(raw) > 0, raw
