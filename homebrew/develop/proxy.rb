27882fb2a6005905ce9895d251e6965b3d41df7a
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-01 20:00:00 -0800

    depends on python

--- homebrew/develop/proxy.rb
+++ homebrew/develop/proxy.rb
@@ -7,7 +7,7 @@ class Proxy < Formula
   url "https://github.com/abhinavsingh/proxy.py/archive/develop.zip"
   version "develop"
 
-  depends_on :python3
+  depends_on "python"
 
   resource "typing-extensions" do
     url "https://files.pythonhosted.org/packages/e7/dd/f1713bc6638cc3a6a23735eff6ee09393b44b96176d3296693ada272a80b/typing_extensions-3.7.4.1.tar.gz"
