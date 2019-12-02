3f7ab0e34a6494b01f7a92b86c2f8a8ec5ab0e8e
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-01 16:47:53 -0800

    Prepend site-packages

--- homebrew/develop/proxy.rb
+++ homebrew/develop/proxy.rb
@@ -8,6 +8,8 @@ class Proxy < Formula
   depends_on "python"
 
   def install
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
     system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
