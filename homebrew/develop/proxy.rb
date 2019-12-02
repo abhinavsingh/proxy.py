e5aae82793cd7d3aefffac655a7619bddab90995
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-01 18:16:19 -0800

    Install typing-extensions explicitly with brew

--- homebrew/develop/proxy.rb
+++ homebrew/develop/proxy.rb
@@ -7,10 +7,22 @@ class Proxy < Formula
 
   depends_on "python"
 
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/e7/dd/f1713bc6638cc3a6a23735eff6ee09393b44b96176d3296693ada272a80b/typing_extensions-3.7.4.1.tar.gz"
+    sha256 "091ecc894d5e908ac75209f10d5b4f118fbdb2eb1ede6a63544054bb1edb41f2"
+  end
+
   def install
     xy = Language::Python.major_minor_version "python3"
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
+    resource("typing-extensions").stage do
+      system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+    end
+
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
     system "python3", *Language::Python.setup_install_args(libexec)
+
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
