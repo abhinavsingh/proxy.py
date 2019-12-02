ab2810cb7b3e27f139ec9dd8c21df40142daaad6
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-01 22:09:52 -0800

    Refactor into inspector module

---
+++ proxy/http/inspector/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+"""
+from .devtools import DevtoolsProtocolPlugin
+
+__all__ = [
+    'DevtoolsProtocolPlugin',
+]
