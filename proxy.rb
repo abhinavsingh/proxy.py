a421860f6de1dd777a7875abd35415a0795f2862
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-01 16:16:58 -0800

    Add homebrew formula

---
+++ proxy.rb
@@ -0,0 +1,15 @@
+class Proxy < Formula
+  desc "⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging."
+  homepage "https://github.com/abhinavsingh/proxy.py"
+  url "https://github.com/abhinavsingh/proxy.py/archive/master.zip"
+  version "1.1.1"
+
+  depends_on "python"
+
+  def install
+    system "python3", *Language::Python.setup_install_args(libexec)
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+end
