ca5be211076bcd832cd23a4347714811548143b8
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-25 16:36:37 -0800

    wait for server to accept requests

--- tests/integration/main.sh
+++ tests/integration/main.sh
@@ -19,6 +19,20 @@ while true; do
     fi
 done
 
+# Wait for http proxy and web server to start
+while true; do
+    curl -v \
+        --max-time 1 \
+        --connect-timeout 1 \
+        -x localhost:8899 \
+        http://localhost:8899/ 2>/dev/null
+    if [[ $? == 0 ]]; then
+        break
+    fi
+    echo "Waiting for web server to start accepting requests..."
+    sleep 1
+done
+
 # Check if proxy was started with integration
 # testing web server plugin.  If detected, use
 # internal web server for integration testing.
@@ -26,19 +40,25 @@ done
 # If integration testing plugin is not found,
 # detect if we have internet access.  If we do,
 # then use httpbin.org for integration testing.
-curl -v -x localhost:8899 http://httpbin.org/get
+curl -v \
+    -x localhost:8899 \
+    http://httpbin.org/get
 if [[ $? != 0 ]]; then
     echo "http request failed"
     exit 1
 fi
 
-curl -v -x localhost:8899 https://httpbin.org/get
+curl -v \
+    -x localhost:8899 \
+    https://httpbin.org/get
 if [[ $? != 0 ]]; then
     echo "https request failed"
     exit 1
 fi
 
-curl -v -x localhost:8899 http://localhost:8899/
+curl -v \
+    -x localhost:8899 \
+    http://localhost:8899/
 if [[ $? != 0 ]]; then
     echo "http request to built in webserver failed"
     exit 1
