7d7149a3259c5766e6584d0afebc5c6273e94d84
Pascal COMBES <pascom@orange.fr>
2020-08-18 14:21:33 +0200

    Implementation on enabled feature.
    Cache is enabled by default.

--- proxy/plugin/cache/base.py
+++ proxy/plugin/cache/base.py
@@ -9,6 +9,7 @@
     :license: BSD, see LICENSE for more details.
 """
 import logging
+import multiprocessing
 from typing import Optional, Any
 
 from .store.base import CacheStore
@@ -31,18 +32,37 @@ class BaseCacheResponsesPlugin(HttpProxyBasePlugin):
     Different storage backends can be used per request if required.
     """
 
+    class EnabledDescriptor:
+        def __init__(self, enabled: bool = False) -> None:
+            self.enabled = multiprocessing.Event()
+            if enabled:
+                self.enabled.set()
+
+        def __get__(self, obj: Optional[object], owner: type) -> Any:
+            if obj is None:
+                return self.enabled
+            return None
+
+    # Dynamically enable / disable cache
+    enabled = EnabledDescriptor(True)
+
     def __init__(
             self,
             *args: Any,
             **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.store: Optional[CacheStore] = None
+        self.__enabled: Optional[bool] = None
 
     def set_store(self, store: CacheStore) -> None:
         self.store = store
 
     def before_upstream_connection(
             self, request: HttpParser) -> Optional[HttpParser]:
+        self.__enabled = self.__class__.enabled.is_set()
+        if not self.__enabled:
+            return request
+
         assert self.store
         logger.info("Upstream connexion %s:%d %s" %
                     (text_(request.host), request.port if request.port else 0, text_(request.path)))
@@ -60,6 +80,10 @@ class BaseCacheResponsesPlugin(HttpProxyBasePlugin):
 
     def handle_client_request(
             self, request: HttpParser) -> Optional[HttpParser]:
+        assert self.__enabled is not None
+        if not self.__enabled:
+            return request
+
         assert self.store
         logger.info("Client request %s:%d %s" %
                     (text_(request.host), request.port if request.port else 0, text_(request.path)))
@@ -100,9 +124,17 @@ class BaseCacheResponsesPlugin(HttpProxyBasePlugin):
         return request
 
     def handle_upstream_chunk(self, chunk: memoryview) -> memoryview:
+        assert self.__enabled is not None
+        if not self.__enabled:
+            return chunk
+
         assert self.store
         return self.store.cache_response_chunk(chunk)
 
     def on_upstream_connection_close(self) -> None:
+        assert self.__enabled is not None
+        if not self.__enabled:
+            return
+
         assert self.store
         self.store.close()
