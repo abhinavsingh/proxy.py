0b844b421804744f32e70bd5dca5052e6fc4d1b9
vzaznobin <Vasiliy Zaznobin>
2021-05-13 12:23:11 +0300

    Remove the hack to create cybercoredev/proxy:latest

--- .buildkite/steps/upload-image.sh
+++ .buildkite/steps/upload-image.sh
@@ -8,10 +8,3 @@ docker images
 docker login -u=${DHUBU} -p=${DHUBP}
 
 docker push cybercoredev/proxy:${REVISION}
-
-if [[ ${BUILDKITE_BRANCH} == "40-proxy-docker" ]]; then
-    docker tag cybercoredev/proxy:${REVISION} cybercoredev/proxy:latest
-    docker push cybercoredev/proxy:latest
-fi
-
-
