5afff66d264a6a899cb61c7a31e6d08686e0133f
vasiliy-zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-05-14 11:30:21 +0300

    Add some changes to check build image time

--- proxy/deploy-test.sh
+++ proxy/deploy-test.sh
@@ -5,5 +5,7 @@ echo "Deploy test..."
 
 curl -v --header "Content-Type: application/json" --data '{"method":"eth_blockNumber","params":[],"id":93,"jsonrpc":"2.0"}' $PROXY_URL
 
+echo "add your tests there"
+
 echo "Deploy test success"
 exit 0
