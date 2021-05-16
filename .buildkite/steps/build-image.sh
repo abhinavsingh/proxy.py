f981c4b90629c997b6c5e41ff0bb41b16fcd03ed
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-05-16 15:46:34 +0300

    Refreshing cybercoredev/solana:latest image is required to run .buildkite/steps/build-image.sh locally Refreshing cybercoredev/evm_loader:latest image is required to run .buildkite/steps/build-image.sh locally

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,4 +3,10 @@ set -euo pipefail
 
 REVISION=$(git rev-parse HEAD)
 
+# Refreshing cybercoredev/solana:latest image is required to run .buildkite/steps/build-image.sh locally
+docker pull cybercoredev/solana:latest
+
+# Refreshing cybercoredev/evm_loader:latest image is required to run .buildkite/steps/build-image.sh locally
+docker pull cybercoredev/evm_loader:latest
+
 docker build -t cybercoredev/proxy:${REVISION} .
