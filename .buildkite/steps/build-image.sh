547c052c2170df3b8023615c19321d01d81bea06
Semen Medvedev <smedvedev@golos.io>
2021-05-18 12:00:11 +0700

    Pass env to docker build #70

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 REVISION=$(git rev-parse HEAD)
 
-set ${SOLANA_REVISION:=latest}
-set ${EVM_LOADER_REVISION:=latest}
+export ${SOLANA_REVISION:=latest}
+export ${EVM_LOADER_REVISION:=latest}
 
 # Refreshing cybercoredev/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull cybercoredev/solana:${SOLANA_REVISION}
