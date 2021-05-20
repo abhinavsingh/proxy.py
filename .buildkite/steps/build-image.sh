7486a416a2420381cc9a8c89993eb61f680c6c6e
Dmitriy Borisenko <otselnik@yandex.ru>
2021-05-20 15:05:34 +0300

    use latest docker image for evm_loader because it is in develop already

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REVISION=$(git rev-parse HEAD)
 
 set ${SOLANA_REVISION:=v1.6.9-resources}
-set ${EVM_LOADER_REVISION:=7d873f6d9b00feba7f80dcad5a4e74023ab3b80c}
+set ${EVM_LOADER_REVISION:=latest}
 
 # Refreshing cybercoredev/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull cybercoredev/solana:${SOLANA_REVISION}
