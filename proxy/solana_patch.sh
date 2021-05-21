e59444be64ee2c472d1e1af0a0069fef9371e2f1
sinev-valentine <sinev-valentine@yandex.ru>
2021-05-21 14:25:07 +0300

    apply comment

--- proxy/solana_patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-patch_file=/usr/local/lib/python3.8/dist-packages/solana/rpc/api.py
-sed -i 's/tx_sig: str, encoding: str = \"json\")/tx_sig: str, encoding: str = \"json\", commitment = \"confirmed\")/g' $patch_file
-sed -i 's/tx_sig, encoding/tx_sig, \{self._comm_key: commitment, self._encoding_key: encoding\}/g' $patch_file
-echo $patch_file   ... PATCHED
