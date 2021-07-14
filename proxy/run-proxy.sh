714be58056a8fd77e8ae2a3c8240f1f4953438dd
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-07-14 20:34:58 +0300

    Fix the review remarks

--- proxy/run-proxy.sh
+++ proxy/run-proxy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+set -xeuo pipefail
 
 date
 
 echo SOLANA_URL=$SOLANA_URL
-
-solana-keygen new --no-passphrase
 
 solana config set -u $SOLANA_URL
 
@@ -15,28 +14,35 @@ for i in {1..10}; do
     sleep 2
 done
 
+solana address || solana-keygen new --no-passphrase
+echo "The following wallet will be used:" && solana address
+
 export EVM_LOADER_TEST_NET_ID="eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU"
 
-if [ "${EVM_LOADER}" == "deploy" ]; then
-  echo "EVM_LOADER is set to load"
-  echo "A new Neon-evm will be deployed"
-  echo airdropping...
-  solana airdrop 1000
-  # check that balance > 20 otherwise airdroping by 1 SOL up to 20
-  set `solana balance|tr '.' ' '`
-  while [ "$1" -lt 20 ]; do solana airdrop 1; sleep 1; set `solana balance|tr '.' ' '`; done
+echo "airdropping..."
+solana airdrop 1000
+# check that balance >= 10 otherwise airdroping by 1 SOL up to 10
+BALANCE=`solana balance | tr '.' '\t'| tr '[:space:]' '\t' | cut -f1`
+while [ "$BALANCE" -lt 10 ]; do
+  solana airdrop 1
+  sleep 1
+  BALANCE=`solana balance | tr '.' '\t'| tr '[:space:]' '\t' | cut -f1`
+done
 
-  echo deploying evm_loader...
+if [ "${EVM_LOADER}" == "deploy" ]; then
+  echo "EVM_LOADER is set to load
+        A new Neon-evm will be deployed
+        deploying evm_loader..."
   solana deploy /spl/bin/evm_loader.so > evm_loader_id
   export EVM_LOADER=$(cat evm_loader_id | sed '/Program Id: \([0-9A-Za-z]\+\)/,${s//\1/;b};s/^.*$//;$q1')
 else
   if [ -z "${EVM_LOADER}" ]; then
-    echo "EVM_LOADER is unset or set to the empty string"
-    echo "The pre-deployed Neon-evm will be used"
+    echo "EVM_LOADER is unset or set to the empty string
+          The pre-deployed Neon-evm will be used"
     export EVM_LOADER="$EVM_LOADER_TEST_NET_ID"
   else
-    echo "EVM_LOADER is set"
-    echo "The specified Neon-evm will be used"
+    echo "EVM_LOADER is set
+          The specified Neon-evm will be used"
   fi
 fi
 
