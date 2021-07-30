949c72dac3caf05187fd07f34216c1e1c2979c9d
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-07-30 21:01:58 +0300

    EVM_LOADER_REVISION=latest

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REVISION=$(git rev-parse HEAD)
 
 set ${SOLANA_REVISION:=v1.6.9-resources}
-set ${EVM_LOADER_REVISION:=5189c18508fdf32ae21a7002a9d283a41a22bd6f}
+set ${EVM_LOADER_REVISION:=latest}
 
 # Refreshing cybercoredev/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull cybercoredev/solana:${SOLANA_REVISION}
