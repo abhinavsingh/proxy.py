22136a012599455a2fa487612bae3e591a839ad7
Abhinav Singh <mailsforabhinav@gmail.com>
2021-11-05 08:50:26 +0530

    Update homebrew formulae.  Doesnt seems to work now

--- helper/homebrew/develop/proxy.rb
+++ helper/homebrew/develop/proxy.rb
@@ -9,11 +9,6 @@ class Proxy < Formula
 
   depends_on "python"
 
-  resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/6a/28/d32852f2af6b5ead85d396249d5bdf450833f3a69896d76eb480d9c5e406/typing_extensions-3.7.4.2.tar.gz"
-    sha256 "79ee589a3caca649a9bfd2a8de4709837400dfa00b6cc81962a1e6a1815969ae"
-  end
-
   def install
     virtualenv_install_with_resources
   end
