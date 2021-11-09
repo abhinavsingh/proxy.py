d5aa4cec667dece6ebd79617e9d8c034dabbcbf4
Abhinav Singh <mailsforabhinav@gmail.com>
2021-11-10 04:14:41 +0530

    uff

--- proxy/core/acceptor/acceptor.py
+++ proxy/core/acceptor/acceptor.py
@@ -147,7 +147,7 @@ class Acceptor(multiprocessing.Process):
                 publisher_id=self.__class__.__name__,
             )
             logger.debug(
-                'Started work#{0}.{1} in thread#{1}'.format(
+                'Started work#{0}.{1} in thread#{2}'.format(
                     self.idd, self._total, thread.ident,
                 ),
             )
