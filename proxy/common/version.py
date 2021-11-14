9704988e942a0b6f5e66a6e1276b85c34fa7551c
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-11-14 11:12:54 +0100

    Reuse a pre-existing `version_tuple` from SCM

--- proxy/common/version.py
+++ proxy/common/version.py
@@ -8,24 +8,7 @@
     :copyright: (c) 2013-present by Abhinav Singh and contributors.
     :license: BSD, see LICENSE for more details.
 """
-from typing import Tuple, Union
-
-from ._version import __version__  # noqa: WPS436
-
-
-def _to_int_or_str(inp: str) -> Union[int, str]:
-    try:
-        return int(inp)
-    except ValueError:
-        return inp
-
-
-def _split_version_parts(inp: str) -> Tuple[str, ...]:
-    public_version, _plus, local_version = inp.partition('+')
-    return (*public_version.split('.'), local_version)
-
-
-VERSION = tuple(map(_to_int_or_str, _split_version_parts(__version__)))
+from ._version import __version__, VERSION  # noqa: WPS436
 
 
 __all__ = '__version__', 'VERSION'
