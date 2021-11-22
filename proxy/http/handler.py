d84d3b44876efcfd3843b5f20ea5f4ac1c241028
Abhinav Singh <mailsforabhinav@gmail.com>
2021-11-23 04:13:41 +0530

    `--threaded` needs a new event loop for 3.7 too

--- proxy/http/handler.py
+++ proxy/http/handler.py
@@ -369,7 +369,8 @@ class HttpProtocolHandler(BaseTcpServerHandler):
         This is here just to maintain backward compatibility with threaded mode.
         """
         if sys.version_info.major == 3 and \
-                sys.version_info.minor == 6:
+                sys.version_info.minor in (6, 7):
+            # NOTE: 3.7 on Windows works without new event loop too
             loop = asyncio.new_event_loop()
         else:
             loop = asyncio.get_event_loop_policy().get_event_loop()
