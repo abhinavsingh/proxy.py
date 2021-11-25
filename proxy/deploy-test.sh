024bf02ed5e8266ad735c275ba833f4d31501029
ivandzen <inkapsulator@gmail.com>
2021-11-25 15:43:09 +0300

    fix scripts (#329)
    Co-authored-by: ivanl <ivanl@cqg.com>

--- proxy/deploy-test.sh
+++ proxy/deploy-test.sh
@@ -5,7 +5,7 @@ echo "Deploy test..."
 
 solana config set -u $SOLANA_URL
 solana address || solana-keygen new --no-passphrase
-export $(/spl/bin/neon-cli --url $SOLANA_URL --evm_loader "$EVM_LOADER" neon-elf-params)
+export $(/spl/bin/neon-cli --commitment confirmed --url $SOLANA_URL --evm_loader "$EVM_LOADER" neon-elf-params)
 
 curl -v --header "Content-Type: application/json" --data '{"method":"eth_blockNumber","id":1,"jsonrpc":"2.0","params":[]}' $PROXY_URL
 
--- proxy/run-test-proxy.sh
+++ proxy/run-test-proxy.sh
@@ -9,7 +9,7 @@ solana config set -u $SOLANA_URL
 
 echo "Dumping evm_loader and extracting ELF parameters"
 export EVM_LOADER=$(solana address -k /spl/bin/evm_loader-keypair.json)
-export $(/spl/bin/neon-cli --url $SOLANA_URL --evm_loader="$EVM_LOADER" neon-elf-params)
+export $(/spl/bin/neon-cli --commitment confirmed --url $SOLANA_URL --evm_loader="$EVM_LOADER" neon-elf-params)
 
 /spl/bin/create-test-accounts.sh 1
 
