6aee7786fcde7e395a4a0ef059941c96829fa308
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-11-29 16:36:01 +0100

    Integrate Towncrier configuration (#823)
    * Move changelog out of README into a separate file
    
    * Expose the changelog to Sphinx
    
    * Integrate Towncrier configuration into the repo
    
    * Include the change fragment docs in Sphinx
    
    * Add a config for the Chronographer GitHub App
    
    * Add a change note for PR #823
    
    * Update CHANGELOG.md
    
    Co-authored-by: Abhinav Singh <126065+abhinavsingh@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -118,6 +118,13 @@ else:
 # Usually you set "language" from the command line for these cases.
 language = 'en'
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    'changelog-fragments.d/**',  # Towncrier-managed change notes
+]
+
 
 # -- Options for HTML output -------------------------------------------------
 
