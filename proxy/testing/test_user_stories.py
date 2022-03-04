c845fb80da2b9737e7a514d871c61b2221e8f1cc
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-03-05 00:27:49 +0700

    Fix gas calculation in tests (#614)

--- proxy/testing/test_user_stories.py
+++ proxy/testing/test_user_stories.py
@@ -42,7 +42,7 @@ class TestUserStories(unittest.TestCase):
         print('response:', response)
         estimated_gas = response['result']
 
-        self.assertEqual(estimated_gas, "0x19fb9f0")
+        self.assertEqual(estimated_gas, "0x19f91f0")
 
     def test_02_check_eth_estimateGas_on_deploying_a_contract_with_the_empty_value(self):
         print("https://github.com/neonlabsorg/proxy-model.py/issues/122")
@@ -58,7 +58,7 @@ class TestUserStories(unittest.TestCase):
         print('response:', response)
         estimated_gas = response['result']
 
-        self.assertEqual(estimated_gas, "0x19fb9f0")
+        self.assertEqual(estimated_gas, "0x19f91f0")
 
     def test_03_check_eth_estimateGas_on_deploying_a_contract_with_the_empty_data(self):
         print("https://github.com/neonlabsorg/proxy-model.py/issues/122")
