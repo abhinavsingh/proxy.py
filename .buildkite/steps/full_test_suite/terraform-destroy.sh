0a309521a87c20baa9fa28c35e07247b6a898ea9
Andrey Voloshin <inkognitotester@gmail.com>
2022-04-04 09:58:03 +0300

    handle each potentially dangerous command (#681)
    * handle each potentially dangerous command
    
    * Refactoring
    
    * refactoring
    
    Co-authored-by: Andrey Voloshin <vo@neonlabs.org>

--- .buildkite/steps/full_test_suite/terraform-destroy.sh
+++ .buildkite/steps/full_test_suite/terraform-destroy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-set -euo pipefail
+
+handle_error() {
+  if [[ $? -ne 0 ]]
+  then
+    echo "Interrupt at step. $1"
+    exit 1
+  fi
+}
+
 
 cd .buildkite/steps/full_test_suite
 
@@ -8,6 +16,7 @@ cd .buildkite/steps/full_test_suite
 export SSH_KEY="~/.ssh/ci-stands"
 export ARTIFACTS_LOGS="./logs"
 mkdir -p $ARTIFACTS_LOGS
+handle_error "Failed to create artifacts dir at: '$ARTIFACTS_LOGS'"
 
 
 # solana
@@ -15,7 +24,9 @@ export SOLANA_HOST=`buildkite-agent meta-data get "SOLANA_IP"`
 ssh-keyscan -H $SOLANA_HOST >> ~/.ssh/known_hosts
 echo "Upload logs for service: solana"
 ssh -i ${SSH_KEY} ubuntu@${SOLANA_HOST} 'sudo docker logs solana 2>&1 | pbzip2 > /tmp/solana.log.bz2'
+handle_error "Can't scan host for fingerprint"
 scp -i ${SSH_KEY} ubuntu@${SOLANA_HOST}:/tmp/solana.log.bz2 ${ARTIFACTS_LOGS}
+handle_error "Retrieve log file for atrifact"
 
 
 # proxy
@@ -27,7 +38,9 @@ for service in "${services[@]}"
 do
    echo "Upload logs for service: $service"
    ssh -i ${SSH_KEY} ubuntu@${PROXY_HOST} "sudo docker logs $service 2>&1 | pbzip2 > /tmp/$service.log.bz2"
+   handle_error "Dump $service log to the file"
    scp -i ${SSH_KEY} ubuntu@${PROXY_HOST}:/tmp/$service.log.bz2 ${ARTIFACTS_LOGS}
+   handle_error "Retrieve log file from service $service"
 done
 
 export NEON_EVM_COMMIT=${NEON_EVM_COMMIT:-latest}
@@ -42,7 +55,9 @@ export TFSTATE_KEY="tests/test-$BUILDKITE_COMMIT"
 export TFSTATE_REGION="us-east-2"
 export TF_BACKEND_CONFIG="-backend-config="bucket=${TFSTATE_BUCKET}" -backend-config="key=${TFSTATE_KEY}" -backend-config="region=${TFSTATE_REGION}""
 terraform init $TF_BACKEND_CONFIG
+handle_error "Failed to proceed the terraform init step. TF_BACKEND_CONFIG: ${TF_BACKEND_CONFIG}"
 terraform destroy --auto-approve=true
+handle_error "Failed to proceed terraform destroy step"
 
 
 # info
