2e66848d29faa6eaa55465d73c64d8c0433229f2
LmR <letmer00t@gmail.com>
2022-04-20 16:03:54 +0200

    Implement the "whitelist" logic for the plugin "FilterByClientIpPlugin"

--- proxy/plugin/filter_by_client_ip.py
+++ proxy/plugin/filter_by_client_ip.py
@@ -20,6 +20,12 @@ from ..common.flag import flags
 from ..http.parser import HttpParser
 from ..http.exception import HttpRequestRejected
 
+flags.add_argument(
+    '--filtered-client-ips-mode',
+    type=str,
+    default='blacklist',
+    help='Default: blacklist.  Can be either "whitelist" (restrict access to specific IPs) or "blacklist" (allow everything except specific IPs).',
+)
 
 flags.add_argument(
     '--filtered-client-ips',
@@ -30,15 +36,22 @@ flags.add_argument(
 
 
 class FilterByClientIpPlugin(HttpProxyBasePlugin):
-    """Drop traffic by inspecting incoming client IP address."""
+    """Allow only (whitelist) or Drop only (blacklist) traffic by inspecting incoming client IP address."""
 
     def before_upstream_connection(
             self, request: HttpParser,
     ) -> Optional[HttpParser]:
-        assert not self.flags.unix_socket_path and self.client.addr
-        if self.client.addr[0] in self.flags.filtered_client_ips.split(','):
-            raise HttpRequestRejected(
-                status_code=httpStatusCodes.I_AM_A_TEAPOT,
-                reason=b'I\'m a tea pot',
-            )
+        assert not self.flags.unix_socket_path and self.client.addr and self.flags.filtered_client_ips_mode in ("blacklist","whitelist")
+        if self.flags.filtered_client_ips_mode == "blacklist":
+            if self.client.addr[0] in self.flags.filtered_client_ips.split(','):
+                raise HttpRequestRejected(
+                    status_code=httpStatusCodes.I_AM_A_TEAPOT,
+                    reason=b'I\'m a tea pot',
+                )
+        elif self.flags.filtered_client_ips_mode == "whitelist":
+            if self.client.addr[0] not in self.flags.filtered_client_ips.split(','):
+                raise HttpRequestRejected(
+                    status_code=httpStatusCodes.I_AM_A_TEAPOT,
+                    reason=b'I\'m a tea pot',
+                )
         return request
