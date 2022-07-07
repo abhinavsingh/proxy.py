262c505c91dc52d42d8a7499632123528facb61e
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-07-07 11:28:36 +0300

    Declare and use variable `MAX_EVM_STEPS_TO_EXECUTE` (#825)
    Emulator restricts number of steps to execute in a single run. This protects proxy from entering infinite loop in case of gas limit exceeded. This parameter was hardcoded as 100_000.
    
    An option --max_steps_to_execute has been added to the command emulate of the neon-cli program.
    
    Corresponding environment variable MAX_EVM_STEPS_TO_EXECUTE has been created for using this option.

--- proxy/common_neon/emulator_interactor.py
+++ proxy/common_neon/emulator_interactor.py
@@ -6,6 +6,7 @@ from typing import Optional, Dict, Any
 from ..common_neon.eth_proto import Trx as NeonTrx
 
 from ..common_neon.elf_params import ElfParams
+from ..common_neon.environment_data import MAX_EVM_STEPS_TO_EXECUTE
 
 from .environment_utils import neon_cli
 from .errors import EthereumError
@@ -294,7 +295,15 @@ def emulator(contract, sender, data, value):
     try:
         neon_token_mint = ElfParams().neon_token_mint
         chain_id = ElfParams().chain_id
-        return neon_cli().call("emulate", "--token_mint", str(neon_token_mint), "--chain_id", str(chain_id), sender, contract, data, value)
+        max_evm_steps_to_execute = MAX_EVM_STEPS_TO_EXECUTE
+        return neon_cli().call("emulate",
+                               "--token_mint", str(neon_token_mint),
+                               "--chain_id", str(chain_id),
+                               "--max_steps_to_execute", str(max_evm_steps_to_execute),
+                               sender,
+                               contract,
+                               data,
+                               value)
     except subprocess.CalledProcessError as err:
         msg, code = NeonCliErrorParser().execute('emulator', err)
         raise EthereumError(message=msg, code=code)
