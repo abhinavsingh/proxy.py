bdd9746fd88ee337cfe102b0569a23e40c3aaafc
Abhinav Singh <mailsforabhinav@gmail.com>
2022-10-22 09:06:41 +0530

    Remove print statements

--- proxy/http/server/reverse.py
+++ proxy/http/server/reverse.py
@@ -13,13 +13,13 @@ import random
 import logging
 from typing import TYPE_CHECKING, Any, Dict, List, Tuple, Optional
 
-from . import HttpWebServerBasePlugin, httpProtocolTypes
-from .. import Url
-from ..parser import HttpParser
-from ..exception import HttpProtocolException
-from ...core.base import TcpUpstreamConnectionHandler
-from ...common.utils import text_
-from ...common.constants import (
+from proxy.http.server import HttpWebServerBasePlugin, httpProtocolTypes
+from proxy.http import Url
+from proxy.http.parser import HttpParser
+from proxy.http.exception import HttpProtocolException
+from proxy.core.base import TcpUpstreamConnectionHandler
+from proxy.common.utils import text_
+from proxy.common.constants import (
     HTTPS_PROTO, DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT,
     DEFAULT_REVERSE_PROXY_ACCESS_LOG_FORMAT,
 )
@@ -94,7 +94,6 @@ class ReverseProxy(TcpUpstreamConnectionHandler, HttpWebServerBasePlugin):
         try:
             self.upstream.connect()
             if self.choice.scheme == HTTPS_PROTO:
-                print(f'=-=-=-=-=-=-=-=-=-=-=- {self.flags.ca_file}')
                 self.upstream.wrap(
                     text_(
                         self.choice.hostname,
