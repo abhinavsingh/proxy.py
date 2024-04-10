8424e91a66dde596b1b0b2429712bca52d5207f7
Abhinav Singh <mailsforabhinav@gmail.com>
2024-04-11 00:03:03 +0530

    Remove blacksheep dep

--- benchmark/_blacksheep.py
+++ benchmark/_blacksheep.py
@@ -9,16 +9,17 @@
     :license: BSD, see LICENSE for more details.
 """
 import uvicorn
-from blacksheep.server import Application
-from blacksheep.server.responses import text
+# from blacksheep.server import Application
+# from blacksheep.server.responses import text
 
 
-app = Application()
+# app = Application()
 
 
-@app.route('/http-route-example')
+# @app.route('/http-route-example')
 async def home(request):    # type: ignore[no-untyped-def]
-    return text('HTTP route response')
+    # return text('HTTP route response')
+    pass
 
 if __name__ == '__main__':
     uvicorn.run('server:app', port=9000, workers=10, log_level='warning')
