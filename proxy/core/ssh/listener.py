b3f51428c0154df3b406711740d3e3f713c5cace
Abhinav Singh <mailsforabhinav@gmail.com>
2024-04-23 20:17:19 +0530

    SSH handler/listener plugins

--- proxy/core/ssh/listener.py
+++ proxy/core/ssh/listener.py
@@ -8,21 +8,20 @@
     :copyright: (c) 2013-present by Abhinav Singh and contributors.
     :license: BSD, see LICENSE for more details.
 """
+import sys
+import socket
 import logging
 import argparse
-from typing import TYPE_CHECKING, Any, Set, Callable, Optional
+from typing import TYPE_CHECKING, Any, Set, Optional, cast
 
 
 try:
-    from paramiko import SSHClient, AutoAddPolicy
-    from paramiko.transport import Transport
-    if TYPE_CHECKING:   # pragma: no cover
-        from paramiko.channel import Channel
-
+    if TYPE_CHECKING:  # pragma: no cover
         from ...common.types import HostPort
 except ImportError:     # pragma: no cover
     pass
 
+from .base import BaseSshTunnelHandler, BaseSshTunnelListener
 from ...common.flag import flags
 
 
@@ -72,18 +71,27 @@ flags.add_argument(
 )
 
 
-class SshTunnelListener:
+class SshTunnelListener(BaseSshTunnelListener):
     """Connects over SSH and forwards a remote port to local host.
 
     Incoming connections are delegated to provided callback."""
 
     def __init__(
-            self,
-            flags: argparse.Namespace,
-            on_connection_callback: Callable[['Channel', 'HostPort', 'HostPort'], None],
+        self,
+        flags: argparse.Namespace,
+        handler: BaseSshTunnelHandler,
+        *args: Any,
+        **kwargs: Any,
     ) -> None:
+        paramiko_logger = logging.getLogger('paramiko')
+        paramiko_logger.setLevel(logging.WARNING)
+
+        # pylint: disable=import-outside-toplevel
+        from paramiko import SSHClient
+        from paramiko.transport import Transport
+
         self.flags = flags
-        self.on_connection_callback = on_connection_callback
+        self.handler = handler
         self.ssh: Optional[SSHClient] = None
         self.transport: Optional[Transport] = None
         self.forwarded: Set['HostPort'] = set()
@@ -92,24 +100,20 @@ class SshTunnelListener:
         assert self.transport is not None
         self.transport.request_port_forward(
             *remote_addr,
-            handler=self.on_connection_callback,
+            handler=self.handler.on_connection,
         )
         self.forwarded.add(remote_addr)
-        logger.info('%s:%d forwarding successful...' % remote_addr)
+        logger.debug("%s:%d forwarding successful..." % remote_addr)
 
     def stop_port_forward(self, remote_addr: 'HostPort') -> None:
         assert self.transport is not None
         self.transport.cancel_port_forward(*remote_addr)
         self.forwarded.remove(remote_addr)
 
-    def __enter__(self) -> 'SshTunnelListener':
-        self.setup()
-        return self
-
-    def __exit__(self, *args: Any) -> None:
-        self.shutdown()
-
     def setup(self) -> None:
+        # pylint: disable=import-outside-toplevel
+        from paramiko import SSHClient, AutoAddPolicy
+
         self.ssh = SSHClient()
         self.ssh.load_system_host_keys()
         self.ssh.set_missing_host_key_policy(AutoAddPolicy())
@@ -119,14 +123,30 @@ class SshTunnelListener:
             username=self.flags.tunnel_username,
             key_filename=self.flags.tunnel_ssh_key,
             passphrase=self.flags.tunnel_ssh_key_passphrase,
+            compress=True,
+            timeout=10,
+            auth_timeout=7,
         )
-        logger.info(
-            'SSH connection established to %s:%d...' % (
+        logger.debug(
+            "SSH connection established to %s:%d..."
+            % (
                 self.flags.tunnel_hostname,
                 self.flags.tunnel_port,
             ),
         )
         self.transport = self.ssh.get_transport()
+        assert self.transport
+        sock = cast(socket.socket, self.transport.sock)  # type: ignore[redundant-cast]
+        # Enable TCP keep-alive
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        # Keep-alive interval (in seconds)
+        if sys.platform != 'darwin':
+            sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 30)
+        # Keep-alive probe interval (in seconds)
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 5)
+        # Number of keep-alive probes before timeout
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 5)
+        self.start_port_forward(('', self.flags.tunnel_remote_port))
 
     def shutdown(self) -> None:
         for remote_addr in list(self.forwarded):
@@ -136,3 +156,10 @@ class SshTunnelListener:
             self.transport.close()
         if self.ssh is not None:
             self.ssh.close()
+        self.handler.shutdown()
+
+    def is_alive(self) -> bool:
+        return self.transport.is_alive() if self.transport else False
+
+    def is_active(self) -> bool:
+        return self.transport.is_active() if self.transport else False
